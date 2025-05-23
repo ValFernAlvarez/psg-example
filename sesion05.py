4e5279b5c41feee7aeeafce6dc8f916b34373b8e
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-23 08:48:38 -0400

    Clase sesión 05

---
+++ sesion05.py
@@ -0,0 +1,71 @@
+'''
+print(type(1))
+#----
+1variable=1
+mas@=1
+class=1
+#---
+print(10)
+print(type(10))
+
+variable=100
+print(variable)
+print(type (variable))
+
+variable_2=int(20)
+print(variable_2)
+print(type (variable_2))
+#---
+print("Base decimal")
+print(10)
+print("Base binaria")
+print(0b1010)
+print("Base octal")
+print(0o12)
+print("Base hexadecimal")
+print(0xa)
+#---
+variable_3=123456789012345678901234567890123456789012345678901234567890
+print(variable_3)
+print(type(variable_3))
+#---
+print(0.5)
+print(type(0.5))
+
+variable_4=0.100546
+print(variable_4)
+print(type(variable_4))
+
+variable_7 =float(1)
+print(variable_7)
+print (type(variable_7))
+
+variable_6=2.0e-3
+print(variable_6)
+print(type(variable_6))
+#---
+min=300
+seg=3600
+hr_mn=300/60
+hr_seg=(3600/60)/60
+print("Total: ",hr_mn+hr_seg)
+#---
+print("Operadores de comparación")
+comparar=10
+print(comparar<10)
+print(comparar>10)
+print(comparar==10)
+print(comparar<=10)
+print(comparar>=10)
+print(comparar!=10)
+'''
+#---
+print("Operadores de comparación")
+comparar=10
+flotante=10.0
+print(comparar<flotante)
+print(comparar>flotante)
+print(comparar==flotante)
+print(comparar<=flotante)
+print(comparar>=flotante)
+print(comparar!=flotante)
