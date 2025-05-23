120a34962e19bab52ff5cc147532f8a465237b50
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-23 09:59:19 -0400

    Volumen de un cubo.

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,9 @@
+'''
+4. Calcular el volumen de un cubo donde uno de sus lados
+    mide 15 metros, utilizando la fórmula general del volumen
+    de un cubo.
+'''
+lado=15
+volumen=lado**3
+#---
+print(f"Volumen del cubo: {volumen} m.")
