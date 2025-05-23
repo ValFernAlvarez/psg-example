a9b788ae60db00e590fb2deac59ee2ee5c898be6
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-23 08:57:01 -0400

    Compra de productos.

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,9 @@
+'''
+Escribe un programa en Python que calcule el total a
+pagar si se compran 12 productos iguales donde cada
+uno cuesta 3.75 USD.
+'''
+cnt_prdct=12 # Cantidad del producto
+precio=3.75 # Precio del producto
+total=cnt_prdct*precio
+print(f"Total: {total} USD$.")
