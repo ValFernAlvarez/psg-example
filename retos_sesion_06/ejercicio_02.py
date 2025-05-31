abed39865a8c46cb1ad20de27e5292830162df32
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-31 05:39:19 -0400

    Operador XNOR

---
+++ retos_sesion_06/ejercicio_02.py
@@ -0,0 +1,10 @@
+'''
+2. Construir el operador XNOR en Python
+'''
+print ("Operador XNOR")
+a=True
+b=False
+print((a and b) or (not a and not b))
+a=True
+b=True
+print((a and b) or (not a and not b))
