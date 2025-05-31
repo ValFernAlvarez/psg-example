3190b9633fda60b007dd7283e5d1baa877e60b83
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-31 06:02:22 -0400

    Comparación de números.

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,11 @@
+'''
+5. Comparar los números 44 y 98 , 111 y 333 , comprobar si tienen la misma paridad ambos pares o ambos impares/
+'''
+num_1=44
+num_2=98
+verificacion=(num_1%2)==(num_2%2)
+print("Para 44 y 98: ", verificacion)
+num_3=111
+num_4=333
+verificacion_2=(num_3%2)==(num_4%2)
+print("Para 111 y 333: ", verificacion_2)
