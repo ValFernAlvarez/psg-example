a0bf91537a6da586d5db9051fe3dc48bc794ef63
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-05-31 06:11:09 -0400

    Cuadrado y suma.

---
+++ retos_sesion_06/ejercicio_07.py
@@ -0,0 +1,8 @@
+'''
+7. ¿El cuadrado y la suma de 2 y 2 son iguales?
+'''
+numero=2
+cuadrado=numero**2
+suma=numero+numero
+verificacion= cuadrado==suma
+print(" ¿El cuadrado y la suma de 2 y 2 son iguales?: ",verificacion)
