8343490705bb60bdb1b9f8e4262a05cf292539a0
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-06-06 05:35:42 -0400

    Resta de tres enteros (entradas).

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,10 @@
+'''
+2. Escribe un programa que reciba tres enteros por teclado y reste su resultado
+'''
+numero_1=int(input("Ingrese el primer entero: "))
+numero_2=int(input("Ingrese el segundo entero: "))
+numero_3=int(input("Ingrese el tercer entero: "))
+#---
+resultado=numero_1-numero_2-numero_3
+#---
+print(f"El resultado de la resta:  {numero_1}-{numero_2}-{numero_3} = {resultado}")
