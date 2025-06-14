1ce9594aa2077b6e61a3dd663dcf67c328f51c6b
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-06-14 05:35:21 -0400

    Coordenadas

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,12 @@
+'''
+6. De las tuplas de coordenadas:
+    $(x1,y1) = (-20, -20) $ y $ (x2,y2) = (40, 20)$
+Encuentra las coordenadas del punto medio, almacena en una tupla e imprime el resultado
+$(x,y)$
+'''
+x1,y1=(-20, -20)
+x2,y2= (40, 20)
+x=(x1+x2)/2
+y=(y1+y2)/2
+punto_medio=(x,y)
+print(f"Punto medio: {punto_medio}")
