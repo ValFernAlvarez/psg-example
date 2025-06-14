09a09dafd1d73d77a29a4ddfb49d05799e5e1a0e
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-06-14 05:28:09 -0400

    Tupla anidada

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,10 @@
+'''
+5. De la siguiente guía telefónica
+    - Jhon, 591123456, Villa fátima
+    - Jane, 591546324, Obrajes
+    - Dogg, 591222421, Miraflores
+    - Kity, 591753159, Achumani
+Genera una tupla anidada e imprime el resultado.
+'''
+guia_telefonica=(("Jhon", 591123456, "Villa Fátima"), ("Jane", 591546324, "Obrajes"), ("Dogg", 591222421, "Miraflores"), ("Kity", 591753159, "Achumani"))
+print(f"Guía Telefónica: {guia_telefonica}")
