b3345c963c328f614659dbeba38469841daa1310
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-06-14 04:40:22 -0400

    Coordenadas.

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,9 @@
+'''
+1. Ingresa por teclado tres coordenadas 'x','y','z',
+    Convierte los valores a enteros almacena los 3 valores en una tupla e imprime el resultado.
+'''
+x=int(input("Coordenada x: "))
+y=int(input("Coordenada y: "))
+z=int(input("Coordenada z: "))
+coordenadas=(x,y,z)
+print(f"Coordenadas: {coordenadas}")
