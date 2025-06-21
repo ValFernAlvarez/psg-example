b9ebf20f3856ad29a7e0c957d35aa0fa7d3d98cc
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-06-21 10:16:47 -0400

    Lista de números flotantes y enteros.

---
+++ retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,15 @@
+'''
+1. Crear una lista con 5 números flotantes y 5 enteros y mostrar todos los elementos y sus tipos de datos utilizando sus índices
+'''
+lst_numeros=[0.1,0.2,0.3,0.4,0.5,1,2,3,4,5]
+print(f"Lista de números flotantes y enteros: {lst_numeros} ")
+print(f"Elemento 1: {lst_numeros[0]:<5} Tipo: {type(lst_numeros[0])}")
+print(f"Elemento 2: {lst_numeros[1]:<5} Tipo: {type(lst_numeros[1])}")
+print(f"Elemento 3: {lst_numeros[2]:<5} Tipo: {type(lst_numeros[2])}")
+print(f"Elemento 4: {lst_numeros[3]:<5} Tipo: {type(lst_numeros[3])}")
+print(f"Elemento 5: {lst_numeros[4]:<5} Tipo: {type(lst_numeros[4])}")
+print(f"Elemento 6: {lst_numeros[5]:<5} Tipo: {type(lst_numeros[5])}")
+print(f"Elemento 7: {lst_numeros[6]:<5} Tipo: {type(lst_numeros[6])}")
+print(f"Elemento 8: {lst_numeros[7]:<5} Tipo: {type(lst_numeros[7])}")
+print(f"Elemento 9: {lst_numeros[8]:<5} Tipo: {type(lst_numeros[8])}")
+print(f"Elemento 10: {lst_numeros[9]:<5} Tipo: {type(lst_numeros[9])}")
