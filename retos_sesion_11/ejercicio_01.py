13baa32820d84e16f3f98ccd75506b78265eafac
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-07-12 08:18:01 -0400

    Información de un animal marino.

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,14 @@
+'''
+1. Utiliza un diccionario para almacenar información de un animal marino
+    de un acuario, registra información como especie, habitat, dieta, estado 
+    de salud, edad y en un set los nombre de los responsables de su cuidado.
+'''
+ajolote={
+    "especie": "Ambystoma mexicanum",
+    "hábitat": "Agua dulce, con poca corriente y vegetación abundante",
+    "dieta": "Carnívoro: se alimenta de gusanos, pequeños peces e insectos",
+    "estado de salud": "Saludable",
+    "edad": 2,  #años
+    "responsables": {"Valeria, Mariel"}}
+#---
+print(f"AJOLOTE 🌸:\n{ajolote}")
