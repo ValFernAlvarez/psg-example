b8b4713b81f11b5a2d2edf752020d5ea6d2f0623
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-07-26 08:17:32 -0400

    20 primeros números.

---
+++ retos_sesion_13/ejercicio_02.py
@@ -0,0 +1,10 @@
+'''
+2. Imprimir los 20 primeros números que sean múltiplos de 2 y 5.
+'''
+contador=0
+numero=0
+while contador<20:
+    if numero%2==0 and numero%5==0:
+        contador+=1
+        print(numero,end=", " if contador<20 else "")
+    numero+=1
