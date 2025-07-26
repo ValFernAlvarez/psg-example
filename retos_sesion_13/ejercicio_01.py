29a0f470344ee4ca8bb73faec1412e50bdd19efa
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-07-26 08:07:13 -0400

    Sucesión de Lucas

---
+++ retos_sesion_13/ejercicio_01.py
@@ -0,0 +1,16 @@
+'''
+1. Imprimir los 20 primeros números de la sucesión de Lucas.
+'''
+print("\nSucesión de Lucas")
+#---
+actual=2
+siguiente=1
+#---
+for termino in range (20):
+    print(actual,end=", " if termino<20-1 else " ")
+    temporal=actual
+    actual=siguiente
+    siguiente+=temporal
+
+    
+
