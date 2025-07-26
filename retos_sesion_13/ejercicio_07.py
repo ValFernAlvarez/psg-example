360d936c1876e0e4457f8f200c48c98e6b0181ba
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-07-26 09:32:02 -0400

    Fizz, Buzz, FizzBuzz.

---
+++ retos_sesion_13/ejercicio_07.py
@@ -0,0 +1,13 @@
+'''
+7. Crear una serie de números del 1 al 100, si el número es divisible entre 5 imprimir Fizz,
+    si el número es divisible entre 7 imprimir Buzz, si el número es divisible entre 5 y 7
+    imprimir FizzBuzz
+'''
+for numero in range (1,101):
+    if numero%5==0 and numero%7==0:
+        numero="FizzBuzz"
+    elif numero%5==0:
+        numero="Fizz"
+    elif numero%7==0:
+        numero="Buzz"
+    print(numero,end=" ")
