90135b368ad4401e302c14ba9fab15f1e4dd91f4
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-07-26 09:01:16 -0400

    Palíndromos.

---
+++ retos_sesion_13/ejercicio_04.py
@@ -0,0 +1,13 @@
+'''
+4. Crea un ciclo infinito que reciba una frase por teclado y verifique si la frase es palíndromo.
+    La ejecución termina si la frase ingresada contiene la palabra salir
+'''
+frase=""
+while True:
+    frase=input("Ingrese frase: ").lower()
+    if frase==(frase[::-1]):
+        print(f"{frase}: Es palíndromo.")
+    elif frase=="salir":
+        break
+    else:
+        print(f"{frase}: No es palíndromo.")
