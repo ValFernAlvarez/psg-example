9bcf4c47f9634d849c6b39ac05c47ddc18129f8e
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-08-09 16:39:50 -0400

    ACTUALIZACIÓN: ejercicio_02.py

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -2,9 +2,22 @@
 2. Construir el operador XNOR en Python
 '''
 print ("Operador XNOR")
+
+a = False
+b = False
+print("A: ",a," B: ", b, " Valor: ",(a and b) or (not a and not b))
+#---
+
+a = False
+b = True
+print("A: ",a," B: ", b, "  Valor: ",(a and b) or (not a and not b))
+#---
+
 a=True
 b=False
-print((a and b) or (not a and not b))
+print("A: ",a,"  B: ", b, " Valor: ",(a and b) or (not a and not b))
+#---
+
 a=True
 b=True
-print((a and b) or (not a and not b))
+print("A: ",a,"  B: ", b, "  Valor: ",(a and b) or (not a and not b))
