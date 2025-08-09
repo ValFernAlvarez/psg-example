1c78aa58c28c190541d8c4e2d0d2f0c0ff7ec30c
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-08-09 17:06:05 -0400

    ACTUALIZACIÓN: ejercicio_02.py

--- retos_sesion_08/ejercicio_02.py
+++ retos_sesion_08/ejercicio_02.py
@@ -10,6 +10,6 @@
 letras=('a','b','c','d','e','f','g','h','i','j')
 print(f"Primer elemento: {letras[0]}")
 print(f"Último elemento: {letras[-1]}")
-print(f"Slice 3 al 5: {letras[3:5]}")
+print(f"Slice 3 al 5: {letras[3:6]}")
 print(f"Slice 5 al 9 con pasos de 3: {letras[5:9:3]}")
 print(f"Slice 9 al 0 con pasos de -2: {letras[9:0:-2]}")
