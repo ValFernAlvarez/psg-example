014159baceb52180150e3ea55b16bad21ecddc2c
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-08-09 16:16:08 -0400

    ACTUALIZACIÓN: ejercicio_03.py

--- retos_sesion_05/ejercicio_03.py
+++ retos_sesion_05/ejercicio_03.py
@@ -2,11 +2,24 @@
 3. Escribe un programa en Python que convierta 1000000 de
     segundos en semanas, días, horas, minutos y segundos
 '''
-segundos=1000000
-semanas=segundos//((3600*24)*7)
-dias=segundos%((3600*24)*7)//((3600*24))
-horas=segundos%((3600*24)*7)%((3600*24))//3600
-minutos=(segundos%((3600*24)*7)%((3600*24))%3600)//60
-segundos=(segundos%((3600*24)*7)%((3600*24))%3600)%60
+
+segundos_por_minuto=60
+segundos_por_hora=3600
+horas_por_dia=24
+dias_por_semana=7
+segundos_por_dia=segundos_por_hora * horas_por_dia
+segundos_por_semana=segundos_por_dia * dias_por_semana
 #---
-print(f"Semana(s): {semanas}  Día(s): {dias}  Hora(s): {horas}  Minuto(s): {minutos}  Segundo(s): {segundos}.")
+segundos = 1000000
+#---
+semanas=segundos//(segundos_por_semana)
+dias=segundos%(segundos_por_semana)//((segundos_por_dia))
+horas=segundos%(segundos_por_semana)%(segundos_por_dia)//segundos_por_hora
+minutos=(segundos%(segundos_por_semana)%(segundos_por_dia)%segundos_por_hora)//segundos_por_minuto
+segundos_restantes=(segundos%(segundos_por_semana)%(segundos_por_dia)%segundos_por_hora)%segundos_por_minuto
+#---
+print("Semana(s):", semanas)
+print("Día(s):", dias)
+print("Hora(s):", horas)
+print("Minuto(s):", minutos)
+print("Segundo(s):", segundos_restantes)
